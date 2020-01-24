4253490b91aac36a00b71f875fd4fb4f09643767
Pradas137 <pradas137@gmail.com>
2020-01-24 19:20:28 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('newpost', function () {
+    return view('welcome');
+
+})->middleware('auth');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/usuaris', function () {
+	//query users
+	$users = App\User::all();
+	//render
+    return view('usuaris',["users"=>$users]);
+});
